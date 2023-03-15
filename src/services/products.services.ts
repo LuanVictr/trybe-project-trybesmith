236d51241cdc05bd5fe699e7f5f5114716a5fa65
Luan Victor <luanvictor2108@gmail.com>
2023-03-15 16:58:30 -0300

    Feat: Implementando validacoes da rota /products | Requisito 6

--- src/services/products.services.ts
+++ src/services/products.services.ts
@@ -12,18 +12,21 @@ class ProductsService {
   static validateProduct(product: Product) {
     const productSchemma = Joi.object({
       id: Joi.number(),
-      name: Joi.string().required(),
-      amount: Joi.string().required(),
+      name: Joi.string().min(3).required(),
+      amount: Joi.string().min(3).required(),
     });
     return productSchemma.validate(product);
   }
 
   public async createProduct(product: Product):Promise<Product> {
     const { error } = ProductsService.validateProduct(product);
+    console.log(error);
     if (error) {
       throw Object.assign(
         new Error(error.message),
-        { status: 500 },
+        error.details[0].type === 'string.base' 
+        || error.details[0].type === 'string.min' ? { status: 422 } : { status: 400 }
+        ,
       );
     }
     const createdProductId = await this.model.createProduct(product);
